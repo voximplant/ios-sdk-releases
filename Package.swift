be8d982ad6868ef950338074ec961302e38521ee
Vladimir Korolev <vladimirbrejcha@icloud.com>
2021-06-07 18:00:35 +0300

    Swift Package 2.40.0 release

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-    	.binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.39.0/VoximplantSDK.zip", checksum: "77227c244b1c21f91c9b5f6498acc7898c6f4857dcb08336b3410d307471c045"),
-        .binaryTarget(name: "VoximplantWebRTC", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-webrtc/89.0.1/WebRTC.zip", checksum: "53c9172098ec17c0cef6ab53a31beebe0e80e6fb50a426be3efbd08c808f7ebb"),
+    	.binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.40.0/VoximplantSDK.zip", checksum: "77227c244b1c21f91c9b5f6498acc7898c6f4857dcb08336b3410d307471c045"),
+        .binaryTarget(name: "VoximplantWebRTC", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-webrtc/89.1.0/WebRTC.zip", checksum: "b43962f6a6502311b0df2d39690ef3d061a365f7cf19b09ab47bd851f081a281"),
     ]
 )
 
