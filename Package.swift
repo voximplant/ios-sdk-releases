e3ed6a5f0d0c65f246637704186ab5e2874f6007
Andrey Efremov <yoklmnprst@ya.ru>
2021-10-08 15:13:37 +0300

    Voxmplant 2.44.1 release.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-	.binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.44.0/VoximplantSDK.zip", checksum: "eee2d438d09428b7e09291bc1bc2111a0713eea55ab69a38d6a09cb13392a507"),
+	.binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.44.1/VoximplantSDK.zip", checksum: "cccfd40a9d326f35a130760764ae1f5643da0586d4f0217fd37469213b0aebae"),
         .binaryTarget(name: "VoximplantWebRTC", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-webrtc/93.0.0/WebRTC.zip", checksum: "3b0ef854454116abd586e42d10176153cbdba9ff3cee22130dd499ba93a185d2"),
     ]
 )
