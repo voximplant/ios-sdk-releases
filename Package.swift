d27e6f33dfcc2f64ccb2bd4351fcdace7e75e6b8
YuliaGrigorieva <miss000hara@gmail.com>
2022-03-04 22:05:59 +0300

    Voximplant iOS SDK 2.46.3 release

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.46.2/VoximplantSDK.zip", checksum: "a88e8e6d662fd6bb9008e92e02f1f5f3fe1ab3dc9c85485fbe862889b31446ae"),
+        .binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.46.3/VoximplantSDK.zip", checksum: "26ef332f07601d1da70f900f65c4511815b3d6be5985f52bd0ed9b5cf3239343"),
         .binaryTarget(name: "VoximplantWebRTC", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-webrtc/93.0.0/WebRTC.zip", checksum: "3b0ef854454116abd586e42d10176153cbdba9ff3cee22130dd499ba93a185d2"),
     ]
 )
