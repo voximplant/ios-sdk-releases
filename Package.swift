fc269056579aaaed65580f29f40ed98330dd78f7
YuliaGrigorieva <miss000hara@gmail.com>
2023-06-20 15:02:26 +0300

    Voximplant iOS SDK 2.48.0 release

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "VoximplantSDK",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v11)
     ],
     products: [
     	.library(name: "VoximplantSDK", targets: ["VoximplantSDK", "VoximplantWebRTC"]),
@@ -12,8 +12,8 @@ let package = Package(
     dependencies: [
     ],
     targets: [
-        .binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.47.1/VoximplantSDK.zip", checksum: "0f7c764d7e7cce96e15f7e6360c1ab2c34fe0e529430be8614e5ecb7f1537ac2"),
-        .binaryTarget(name: "VoximplantWebRTC", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-webrtc/93.0.1/WebRTC.zip", checksum: "654b43928c71b2aaf63bde0285afeaa38d460e377d51c551ad482bece0252d48"),
+        .binaryTarget(name: "VoximplantSDK", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/2.48.0/VoximplantSDK.zip", checksum: "a03bb9b5d122155fb0602e17174807f339b27f70046bbafc6e3881adde532fe5"),
+        .binaryTarget(name: "VoximplantWebRTC", url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-webrtc/106.0.0/WebRTC.zip", checksum: "b626a1c18a37213859690b754d1be59e98939989b73886466ffc8a418b9b0c88"),
     ]
 )
 
