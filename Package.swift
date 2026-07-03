eb8dc058b491dd862a5028b11bf4035513ea20db
Vlad Kononenko <vlad.kononenko95@gmail.com>
2026-07-03 20:11:30 +0300

    Voximplant iOS SDK 3.2.0 release

--- Package.swift
+++ Package.swift
@@ -23,16 +23,16 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "VoximplantCore",
-                      url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/3.1.0/VoximplantCore.zip",
-                      checksum: "a681a13a771a16c1fd671b29bf4343a103f3079bb7a946d9d99daf238ee32648"
+                      url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/3.2.0/VoximplantCore.zip",
+                      checksum: "7544932eeec466134c8c1d6ccb421c285f101fc872876d6d28a612bca326929c"
                      ),
         .binaryTarget(name: "VoximplantMessaging",
-                      url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/3.1.0/VoximplantMessaging.zip",
-                      checksum: "243ff00352a1d6c4535e42ec9e518d4230bca720ebcb64415a912209e84b80e2"
+                      url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/3.2.0/VoximplantMessaging.zip",
+                      checksum: "eee2973ed8414f72cb17df345a4281c1590a4f458405e813e5b29fd8cdee0cba"
                      ),
         .binaryTarget(name: "VoximplantCalls",
-                      url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/3.1.0/VoximplantCalls.zip",
-                      checksum: "8800f006f25fdbcee0d365771a6c67dd903d6bff1063339d69af638c975e6b3c"
+                      url: "https://voximplant-releases.s3.eu-central-1.amazonaws.com/ios-sdk/3.2.0/VoximplantCalls.zip",
+                      checksum: "7c68792a8c6183c77a243d36334c90f746078813c9023de3683a1333c21d7ccd"
                      ),
         .binaryTarget(name: "VoximplantWebRTC",
                       url: "https://s3.eu-central-1.amazonaws.com/voximplant-releases/ios-webrtc/144.0.0/WebRTC.zip",
